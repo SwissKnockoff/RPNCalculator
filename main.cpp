2e409a10e8bb624e1030fccc1d659f6a0733c229
SwissKnockoff <jpaol1@ocdsb.ca>
2017-04-04 22:40:36 -0400

    more buttons

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ int main() {
 	SLATE_GRAY = al_map_rgb(112, 138, 144);
 	
 	//Buttons
-	Button *buttons[20];
+	Button *buttons[25];
 	buttons[0] = new Button(0, 7, 1, 1, SQUARE_SIZE, WHITE, "0"); //0
 	buttons[1] = new Button(0, 6, 1, 1, SQUARE_SIZE, WHITE, "1"); //1
 	buttons[2] = new Button(1, 6, 1, 1, SQUARE_SIZE, WHITE, "2"); //2
@@ -49,8 +49,8 @@ int main() {
 	buttons[5] = new Button(1, 5, 1, 1, SQUARE_SIZE, WHITE, "5"); //5
 	buttons[6] = new Button(2, 5, 1, 1, SQUARE_SIZE, WHITE, "6"); //6
 	buttons[7] = new Button(0, 4, 1, 1, SQUARE_SIZE, WHITE, "7"); //7
-	buttons[8] = new Button(1, 4, 1, 1, SQUARE_SIZE, WHITE, "8"); //0
-	buttons[9] = new Button(2, 4, 1, 1, SQUARE_SIZE, WHITE, "9"); //1
+	buttons[8] = new Button(1, 4, 1, 1, SQUARE_SIZE, WHITE, "8"); //8
+	buttons[9] = new Button(2, 4, 1, 1, SQUARE_SIZE, WHITE, "9"); //9
 	buttons[10] = new Button(2, 7, 1, 1, SQUARE_SIZE, WHITE, "+/-"); //Negate
 	buttons[11] = new Button(1, 7, 1, 1, SQUARE_SIZE, WHITE, "."); //Decimal
 	buttons[12] = new Button(3, 4, 1, 1, SQUARE_SIZE, SLATE_GRAY, "+"); //Plus
@@ -58,16 +58,27 @@ int main() {
 	buttons[14] = new Button(3, 5, 1, 1, SQUARE_SIZE, SLATE_GRAY, "X"); //Times
 	buttons[15] = new Button(2, 3, 1, 1, SQUARE_SIZE, SLATE_GRAY, "/"); //Divide
 	buttons[16] = new Button(3, 6, 1, 2, SQUARE_SIZE, SLATE_GRAY, "ENT"); //Enter
+	buttons[17] = new Button(3, 2, 1, 1, SQUARE_SIZE, SLATE_GRAY, "1/x"); //Reciprocal
+	buttons[18] = new Button(0, 3, 1, 1, SQUARE_SIZE, SLATE_GRAY, "SQRT"); //Square Root
+	buttons[19] = new Button(4, 4, 1, 2, SQUARE_SIZE, SLATE_GRAY, "C"); //Clear
+	buttons[20] = new Button(4, 6, 1, 2, SQUARE_SIZE, SLATE_GRAY, "AC"); //All Clear
+	buttons[21] = new Button(0, 2, 1, 1, SQUARE_SIZE, SLATE_GRAY, "sin"); //Sine
+	buttons[22] = new Button(1, 2, 1, 1, SQUARE_SIZE, SLATE_GRAY, "cos"); //Cosine
+	buttons[23] = new Button(2, 2, 1, 1, SQUARE_SIZE, SLATE_GRAY, "tan"); //Tangent
+	buttons[24] = new Button(1, 3, 1, 1, SQUARE_SIZE, SLATE_GRAY, "y^x"); //Exponents
 	
 	Operator calc;
 	
 	while(true) {
 		
-		for(int i = 0; i < 17; i++) {
+		for(int i = 0; i <= 24; i++) {
 			
 			buttons[i]->draw();
 			
 		}
+		
+		al_draw_filled_rectangle(0.05 * SQUARE_SIZE, 0.05 * SQUARE_SIZE, 4 * SQUARE_SIZE + 0.05 * SQUARE_SIZE, 0.95 * SQUARE_SIZE, WHITE);
+		al_draw_filled_rectangle(0.05 * SQUARE_SIZE, SQUARE_SIZE + 0.05 * SQUARE_SIZE, 4 * SQUARE_SIZE + 0.05 * SQUARE_SIZE, SQUARE_SIZE + 0.95 * SQUARE_SIZE, WHITE);
 					
 		al_flip_display();
 		
